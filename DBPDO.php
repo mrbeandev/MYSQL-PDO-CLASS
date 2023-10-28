b799fe0ff67a699d4020a50ac5d467e615330dc5
MrBeanDev <34623886+mrbeandev@users.noreply.github.com>
2023-10-28 07:43:39 +0530

    Update DBPDO.php

--- DBPDO.php
+++ DBPDO.php
@@ -1,27 +1,22 @@
 <?php
 
-/**
-* This Was Forked From https://github.com/a1phanumeric/PHP-MySQL-Class
-* and Modified to make it more usefull.
-* i have also added Error handling for each query.
-*/
-
-class DBPDO {
+class DBPDO
+{
 	public $pdo;
 	private $error;
-	
-	function __construct() {
+	function __construct()
+	{
 		$this->connect();
 	}
-	
-	function prep_query($query){
+	function prep_query($query)
+	{
 		return $this->pdo->prepare($query);
 	}
-	
-	function connect(){
-		if(!$this->pdo){
-			$dsn      = 'mysql:dbname=' . DATABASE_NAME . ';host=' . DATABASE_HOST.';charset=utf8';
-			$user     = DATABASE_USER;
+	function connect()
+	{
+		if (!$this->pdo) {
+			$dsn = 'mysql:dbname=' . DATABASE_NAME . ';host=' . DATABASE_HOST . ';charset=utf8';
+			$user = DATABASE_USER;
 			$password = DATABASE_PASS;
 			try {
 				$this->pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_PERSISTENT => true));
@@ -31,74 +26,77 @@ class DBPDO {
 				$this->error = $e->getMessage();
 				return $this->error;
 			}
-		}else{
-			$this->pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+		} else {
+			$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 			return true;
 		}
 	}
 
-
-	function table_exists($table_name){
-		try{
+	function table_exists($table_name)
+	{
+		try {
 			$stmt = $this->prep_query('SHOW TABLES LIKE ?');
 			$stmt->execute(array($table_name));
 			return $stmt->rowCount() > 0;
-		} catch(PDOException $e){
+		} catch (PDOException $e) {
 			return $e->getMessage();
 		}
 	}
 
-	function execute($query, $values = null){
-		try{
-			if($values == null){
+	function execute($query, $values = null)
+	{
+		try {
+			if ($values == null) {
 				$values = array();
-			}else if(!is_array($values)){
+			} else if (!is_array($values)) {
 				$values = array($values);
 			}
 			$stmt = $this->prep_query($query);
 			$stmt->execute($values);
 			return $stmt;
-		} catch(PDOException $e){
+		} catch (PDOException $e) {
 			return $e->getMessage();
 		}
 	}
-	
-	function count($query, $values = null){
-		try{
-			if($values == null){
+
+	function count($query, $values = null)
+	{
+		try {
+			if ($values == null) {
 				$values = array();
-			}else if(!is_array($values)){
+			} else if (!is_array($values)) {
 				$values = array($values);
 			}
 			$stmt = $this->prep_query($query);
 			$stmt->execute($values);
 			$number = $stmt->rowCount();
 			return $number;
-		} catch(PDOException $e){
+		} catch (PDOException $e) {
 			return $e->getMessage();
 		}
 	}
 
-	// this returns a php object
-	function fetch($query, $values = null){
-		try{
-			if($values == null){
+	function fetch($query, $values = null)
+	{
+		try {
+			if ($values == null) {
 				$values = array();
-			}else if(!is_array($values)){
+			} else if (!is_array($values)) {
 				$values = array($values);
 			}
 			$stmt = $this->execute($query, $values);
-			return $stmt->fetchObject(PDO::FETCH_ASSOC);
-		} catch(PDOException $e){
+			return $stmt->fetchObject();
+		} catch (PDOException $e) {
 			return $e->getMessage();
 		}
 	}
 
-	function fetchAll($query, $values = null, $key = null){
-		try{
-			if($values == null){
+	function fetchall($query, $values = null, $key = null)
+	{
+		try {
+			if ($values == null) {
 				$values = array();
-			}else if(!is_array($values)){
+			} else if (!is_array($values)) {
 				$values = array($values);
 			}
 			$stmt = $this->execute($query, $values);
@@ -106,21 +104,21 @@ class DBPDO {
 
 			// Allows the user to retrieve results using a
 			// column from the results as a key for the array
-			if($key != null && $results[0][$key]){
+			if ($key != null && $results[0][$key]) {
 				$keyed_results = array();
-				foreach($results as $result){
+				foreach ($results as $result) {
 					$keyed_results[$result[$key]] = $result;
 				}
 				$results = $keyed_results;
 			}
 			return $results;
-		} catch(PDOException $e){
+		} catch (PDOException $e) {
 			return $e->getMessage();
 		}
 	}
 
-	function lastInsertId(){
+	function lastInsertId()
+	{
 		return $this->pdo->lastInsertId();
 	}
-
 }
